eec6a86213029e42160ed7188961b5dfd27e0967
Will Blair <wdblair@bu.edu>
2014-04-02 04:17:16 -0400

    Adding new xml files for communicating with flightgear

--- fgfs.sh
+++ fgfs.sh
@@ -5,5 +5,5 @@
 
 #fgfs --altitude=3000 --vc=75 --httpd=5500 \
 fgfs --httpd=5500 \
-    --generic=socket,out,40,localhost,5000,udp,output_protocol \
-    --generic=socket,in,45,localhost,5010,udp,input_protocol
+    --generic=socket,out,10,localhost,5000,udp,sensor_protocol \
+    --generic=socket,in,10,localhost,5010,udp,actuaotr_protocol
